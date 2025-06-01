0a008bc2d6c61833ee57caa329cd9b6ceb585585
nickolss <nickolasmaiaraujo@gmail.com>
2025-06-01 13:32:11 -0300

    feat: added first project structure

---
+++ rest-with-spring-boot/src/main/java/com/nickolss/rest_with_spring_boot/RestWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nickolss.rest_with_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ rest-with-spring-boot/src/test/java/com/nickolss/rest_with_spring_boot/RestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickolss.rest_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
