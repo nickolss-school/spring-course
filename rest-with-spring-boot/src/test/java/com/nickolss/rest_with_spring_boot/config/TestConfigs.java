6c9f48beb9392e9e36d2ff5b4340df85265d4f72
nickolss <nickolasmaiaraujo@gmail.com>
2025-07-08 16:16:59 -0300

    feat: testcontainers class added

---
+++ rest-with-spring-boot/src/test/java/com/nickolss/rest_with_spring_boot/config/TestConfigs.java
@@ -0,0 +1,5 @@
+package com.nickolss.rest_with_spring_boot.config;
+
+public interface TestConfigs {
+    int SERVER_PORT = 8888;
+}
---
+++ rest-with-spring-boot/src/test/java/com/nickolss/rest_with_spring_boot/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.nickolss.rest_with_spring_boot.integrationtests.swagger;
+
+import com.nickolss.rest_with_spring_boot.config.TestConfigs;
+import com.nickolss.rest_with_spring_boot.integrationtests.testcontainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void shouldDisplaySwaggerUIPage() {
+        var content = given()
+                .basePath("/swagger-ui/index.html")
+                    .port(TestConfigs.SERVER_PORT)
+
+                .when()
+                    .get()
+
+                .then()
+                    .statusCode(200)
+
+                .extract()
+                    .body()
+                        .asString();
+
+        assertTrue(content.contains("Swagger UI"));
+
+    }
+
+}
